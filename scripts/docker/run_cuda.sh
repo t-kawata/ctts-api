139b1b31c186231f8db9d4db2bbf292258f49da7
Toshimi Kawata <kawata@shyme.net>
2025-07-03 17:41:23 +0900

    tmp

--- scripts/docker/run_cuda.sh
+++
@@ -1,4 +0,0 @@
-docker run -it --rm -p 3000:3000 --name sbv2 \
--v ./models:/work/models --env-file .env \
---gpus all \
-ghcr.io/neodyland/sbv2-api:cuda
--- scripts/sbv2-test-api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-res = requests.post(
-    "http://localhost:3000/synthesize",
-    json={"text": "おはようございます", "ident": "tsukuyomi"},
-)
-with open("output.wav", "wb") as f:
-    f.write(res.content)
